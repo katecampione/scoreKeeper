864867022d8d9773d445afa8e830b36d5af839dd
Kate Campione <katecampione@gmail.com>
2019-08-29 13:18:57 -0600

    created eventListener for p1 & p2 buttons

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,7 +1,16 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("#reset");
+var p1Display = document.querySelector("#p1Display");
+var p1Score = 0;
+var p2Score = 0;
 
 p1Button.addEventListener("click", function(){
-  alert("clicked!");
+  p1Score++;
+  p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+  p2Score++;
+  console.log(p2Score);
 });
