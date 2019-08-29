f502628760d8759368d0d6a28d49d263de73e5ab
Kate Campione <katecampione@gmail.com>
2019-08-29 13:57:32 -0600

    created gameOver var to stop score

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,16 +1,34 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
-var resetButton = document.getElementById("#reset");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
 var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function(){
-  p1Score++;
-  p1Display.textContent = p1Score;
+  if (!gameOver) {
+    p1Score++;
+    if(p1Score === winningScore){
+      gameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
 });
 
 p2Button.addEventListener("click", function(){
-  p2Score++;
-  console.log(p2Score);
+  if (!gameOver) {
+    p2Score++;
+    if(p2Score === winningScore){
+      gameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
+});
+
+resetButton.addEventListener("click", function(){
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
 });
