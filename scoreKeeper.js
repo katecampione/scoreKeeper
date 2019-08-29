ea0ac33407df6fce42da22059d53be14c9839aca
Kate Campione <katecampione@gmail.com>
2019-08-29 13:03:52 -0600

    created variables for buttons

---
+++ scoreKeeper.js
@@ -0,0 +1,7 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("3p2");
+var resetButton = document.querySelector("#reset");
+
+p1Button.addEventListener("click", function()){
+  alert("clicked!");
+});
