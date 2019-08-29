1d6f66096a2a7bdcf76ee953d4990bfb71fb8fb6
Kate Campione <katecampione@gmail.com>
2019-08-29 14:24:38 -0600

    created event for reset button

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -31,6 +31,10 @@ p2Button.addEventListener("click", function(){
 });
 
 resetButton.addEventListener("click", function(){
-    p1Display.textContent = p1Score;
-    p2Display.textContent = p2Score;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
 });
